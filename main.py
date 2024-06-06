5927bc485b7c78bf67407171a92acfe5131d5d1d
kubuszi <kubushii2@wp.pl>
2024-06-06 17:35:08 +0200

    Scraping data by BeautifulSoup

---
+++ main.py
@@ -0,0 +1,34 @@
+from bs4 import BeautifulSoup
+import requests
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+
+GOOGLE_FORM_LINK = ""
+
+header = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
+    "Accept-Language": "pl-PL,pl;q=0.9,en-US;q=0.8,en;q=0.7"
+}
+
+response = requests.get("https://appbrewery.github.io/Zillow-Clone/", headers=header)
+
+data = response.text
+soup = BeautifulSoup(data, "html.parser")
+
+all_link_elements = soup.select(".StyledPropertyCardDataWrapper a")
+
+all_links = [link["href"] for link in all_link_elements]
+print(f"There are {len(all_links)} links to individual listings in total: \n")
+print(all_links)
+
+all_address_elements = soup.select(".StyledPropertyCardDataWrapper address")
+all_addresses = [address.get_text().replace(" | ", " ").strip() for address in all_address_elements]
+print(f"\n After having been cleaned up, the {len(all_addresses)} addresses now look like this: \n")
+print(all_addresses)
+
+all_price_elements = soup.select(".PropertyCardWrapper span")
+all_prices = [price.get_text().replace("/mo", "").split("+")[0] for price in all_price_elements if "$" in price.text]
+print(f"\n After having been cleaned up, the {len(all_prices)} prices now look like this: \n")
+print(all_prices)
