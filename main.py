fd819b8fb1b5fbf09a44c23215a70ad6df43c325
kubuszi <kubushii2@wp.pl>
2024-06-07 10:09:50 +0200

    Putting data into form

--- main.py
+++ main.py
@@ -32,3 +32,25 @@ all_price_elements = soup.select(".PropertyCardWrapper span")
 all_prices = [price.get_text().replace("/mo", "").split("+")[0] for price in all_price_elements if "$" in price.text]
 print(f"\n After having been cleaned up, the {len(all_prices)} prices now look like this: \n")
 print(all_prices)
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(options=chrome_options)
+
+for n in range(len(all_links)):
+    driver.get(GOOGLE_FORM_LINK)
+    time.sleep(2)
+
+    address = driver.find_element(by=By.XPATH,
+                                  value='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    price = driver.find_element(by=By.XPATH,
+                                value='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    link = driver.find_element(by=By.XPATH,
+                               value='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    submit_button = driver.find_element(by=By.XPATH,
+                                        value='//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div')
+
+    address.send_keys(all_addresses[n])
+    price.send_keys(all_prices[n])
+    link.send_keys(all_links[n])
+    submit_button.click()
